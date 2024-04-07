a8ff2d51ba47aed757ba40297c8a25cd4a0eb843
宮下颯太 <sou@MacBook-Air-M2.local>
2024-04-07 23:38:35 +0900

    version

--- src/ProgrammingAPI.ts
+++ src/ProgrammingAPI.ts
@@ -5,7 +5,7 @@ export const getJavaScriptBooks = async (
   page: number = 1
 ): Promise<ApiResponse> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/javascript?page=${page}`,
+    `http://localhost:3001/api/v1/javascript?page=${page}`,
     {
       next: { revalidate: 1800 },
     }
@@ -19,7 +19,7 @@ export const getTypeScriptBooks = async (
   page: number = 1
 ): Promise<ApiResponse> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/typescript?page=${page}`,
+    `http://localhost:3001/api/v1/typescript?page=${page}`,
     {
       next: { revalidate: 1800 },
     }
@@ -30,24 +30,18 @@ export const getTypeScriptBooks = async (
 
 /* Javaの取得 */
 export const getJavaBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/java?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/java?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const javabooks = await res.json();
   return javabooks;
 };
 
 /* Rubyの取得 */
 export const getRubyBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/ruby?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/ruby?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const rubybooks = await res.json();
   return rubybooks;
 };
@@ -56,84 +50,63 @@ export const getRubyBooks = async (page: number = 1): Promise<ApiResponse> => {
 export const getPythonBooks = async (
   page: number = 1
 ): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/python?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/python?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const pythonbooks = await res.json();
   return pythonbooks;
 };
 
 /* Goの取得 */
 export const getGoBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/go?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/go?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const gobooks = await res.json();
   return gobooks;
 };
 
 /* PHPの取得 */
 export const getPhpBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/php?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/php?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const phpbooks = await res.json();
   return phpbooks;
 };
 
 /* Reactの取得 */
 export const getReactBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/react?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/react?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const reactbooks = await res.json();
   return reactbooks;
 };
 
 /* Vueの取得 */
 export const getVueBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/vue?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/vue?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const vuebooks = await res.json();
   return vuebooks;
 };
 
 /* Next.jsの取得 */
 export const getNextBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/nextjs?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/nextjs?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const nextbooks = await res.json();
   return nextbooks;
 };
 
 /* Nuxt.jsの取得 */
 export const getNuxtBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/nuxtjs?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/nuxtjs?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const nuxtbooks = await res.json();
   return nuxtbooks;
 };
@@ -142,12 +115,9 @@ export const getNuxtBooks = async (page: number = 1): Promise<ApiResponse> => {
 export const getLaravelBooks = async (
   page: number = 1
 ): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/laravel?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/laravel?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const laravelbooks = await res.json();
   return laravelbooks;
 };
@@ -156,24 +126,18 @@ export const getLaravelBooks = async (
 export const getJqueryBooks = async (
   page: number = 1
 ): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/jquery?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/jquery?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const jquerybooks = await res.json();
   return jquerybooks;
 };
 
 /* Railsの取得 */
 export const getRailsBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/rails?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/rails?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const railsbooks = await res.json();
   return railsbooks;
 };
--- src/bookAPI.ts
+++ src/bookAPI.ts
@@ -2,36 +2,27 @@ import { ApiResponse, BookType } from "../types/types";
 
 /* 全ての本 */
 export const getAllBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/search?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/search?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const allbooks = await res.json();
   return allbooks;
 };
 
 /* 基礎本の取得 */
 export const getBaseBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/base?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/base?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const basebooks = await res.json();
   return basebooks;
 };
 
 /*　デザイン本の取得 */
 export const getCssBooks = async (page: number = 1): Promise<ApiResponse> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/design?page=${page}`,
-    {
-      next: { revalidate: 1800 },
-    }
-  );
+  const res = await fetch(`http://localhost:3001/api/v1/design?page=${page}`, {
+    next: { revalidate: 1800 },
+  });
   const cssbooks = await res.json();
   return cssbooks;
 };
@@ -41,7 +32,7 @@ export const getComputerBooks = async (
   page: number = 1
 ): Promise<ApiResponse> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/computer_science?page=${page}`,
+    `http://localhost:3001/api/v1/computer_science?page=${page}`,
     {
       next: { revalidate: 1800 },
     }
@@ -53,7 +44,7 @@ export const getComputerBooks = async (
 /* フロントエンドの取得 */
 export const getFrontBooks = async (page: number = 1): Promise<ApiResponse> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/front_end?page=${page}`,
+    `http://localhost:3001/api/v1/front_end?page=${page}`,
     {
       next: { revalidate: 1800 },
     }
@@ -65,7 +56,7 @@ export const getFrontBooks = async (page: number = 1): Promise<ApiResponse> => {
 /* バックエンドの取得 */
 export const getBackBooks = async (page: number = 1): Promise<ApiResponse> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/back_end?page=${page}`,
+    `http://localhost:3001/api/v1/back_end?page=${page}`,
     {
       next: { revalidate: 1800 },
     }
@@ -77,7 +68,7 @@ export const getBackBooks = async (page: number = 1): Promise<ApiResponse> => {
 /* インフラの取得 */
 export const getInfraBooks = async (page: number = 1): Promise<ApiResponse> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/infrastructure?page=${page}`,
+    `http://localhost:3001/api/v1/infrastructure?page=${page}`,
     {
       next: { revalidate: 1800 },
     }
@@ -89,7 +80,7 @@ export const getInfraBooks = async (page: number = 1): Promise<ApiResponse> => {
 /* Web開発の取得 */
 export const getWebBooks = async (page: number = 1): Promise<ApiResponse> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/web_development?page=${page}`,
+    `http://localhost:3001/api/v1/web_development?page=${page}`,
     {
       next: { revalidate: 1800 },
     }
@@ -103,7 +94,7 @@ export const getUsefulBooks = async (
   page: number = 1
 ): Promise<ApiResponse> => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/useful_book?page=${page}`,
+    `http://localhost:3001/api/v1/useful_book?page=${page}`,
     {
       next: { revalidate: 1800 },
     }
